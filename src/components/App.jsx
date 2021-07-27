58837a8a78da11a5aaf7730b09885a19ad18000b
Ejim Michael <ejimmichael1@gmail.com>
2021-07-27 12:17:20 +0100

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import Heading from './Heading'
+import Header from './Header'
+import Footer from './Footer'
+import Note from './Note'
 
 function App() {
     return (
         <div>
-            <Heading />
+            <Header />
+            <Note />
+            <Footer />
         </div>
     )
 }
 
-export default App;
+export default App
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Note() {
+    return (
+        <div className = "note">
+            <h1>Learning React</h1>
+            <p>I will soon be a pro react developer</p>
+        </div>
+    )
+}
+
+export default Note
