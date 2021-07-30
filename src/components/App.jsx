a2a343631d082256150a48c73d27458d0b2fd962
Ejim Michael <ejimmichael1@gmail.com>
2021-07-30 16:13:06 +0100

    map data to components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,29 @@ import Card from "./Card";
 import contacts from "../contacts";
 import Avatar from "./Avatar"
 
+function createCard(contact) {
+  return (
+  <Card
+  id ={contact.id}
+    key={contact.id}
+    name={contact.name}
+    img={contact.imgURL}
+    tel={contact.phone}
+    email={contact.email}
+  />
+  )
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Avatar 
-        img ="https://upload.wikimedia.org/wikipedia/commons/e/ed/Denzel_Washington_cropped_02.jpg"
+        img ="https://upload.wikimedia.org/wikipedia/commons/e/ed/Denzel_Washington_cropped_02.jpg"  
       />
-      <Card
+      {contacts.map(createCard)}
+
+      {/* <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -27,7 +42,7 @@ function App() {
         img={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
